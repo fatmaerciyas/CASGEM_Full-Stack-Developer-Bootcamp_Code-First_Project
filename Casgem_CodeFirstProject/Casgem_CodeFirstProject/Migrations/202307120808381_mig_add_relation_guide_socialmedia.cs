6c2649e11856f0e56ef7401a7861d578d0f65339
fatmaerciyas <fatmaerciyas22@gmail.com>
2023-07-18 22:06:30 +0300

    chore: created project

---
+++ Casgem_CodeFirstProject/Casgem_CodeFirstProject/Migrations/202307120808381_mig_add_relation_guide_socialmedia.cs
@@ -0,0 +1,22 @@
+﻿namespace Casgem_CodeFirstProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig_add_relation_guide_socialmedia : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.SocialMedias", "GuideID", c => c.Int(nullable: false));
+            CreateIndex("dbo.SocialMedias", "GuideID");
+            AddForeignKey("dbo.SocialMedias", "GuideID", "dbo.Guides", "GuideID", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.SocialMedias", "GuideID", "dbo.Guides");
+            DropIndex("dbo.SocialMedias", new[] { "GuideID" });
+            DropColumn("dbo.SocialMedias", "GuideID");
+        }
+    }
+}
